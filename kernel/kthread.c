4d706308374f6e6b5e2e3d4b15aae1ce49a662f9
David Kershner <david.kershner@unisys.com>
2015-08-06 15:46:45 -0700

    kthread: export kthread functions
    The s-Par visornic driver, currently in staging, processes a queue being
    serviced by the an s-Par service partition.  We can get a message that
    something has happened with the Service Partition, when that happens, we
    must not access the channel until we get a message that the service
    partition is back again.
    
    The visornic driver has a thread for processing the channel, when we get
    the message, we need to be able to park the thread and then resume it
    when the problem clears.
    
    We can do this with kthread_park and unpark but they are not exported
    from the kernel, this patch exports the needed functions.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Neil Horman <nhorman@tuxdriver.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Richard Weinberger <richard.weinberger@gmail.com>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -97,6 +97,7 @@ bool kthread_should_park(void)
 {
 	return test_bit(KTHREAD_SHOULD_PARK, &to_kthread(current)->flags);
 }
+EXPORT_SYMBOL_GPL(kthread_should_park);
 
 /**
  * kthread_freezable_should_stop - should this freezable kthread return now?
@@ -171,6 +172,7 @@ void kthread_parkme(void)
 {
 	__kthread_parkme(to_kthread(current));
 }
+EXPORT_SYMBOL_GPL(kthread_parkme);
 
 static int kthread(void *_create)
 {
@@ -427,6 +429,7 @@ void kthread_unpark(struct task_struct *k)
 		put_task_stack(k);
 	}
 }
+EXPORT_SYMBOL_GPL(kthread_unpark);
 
 /**
  * kthread_park - park a thread created by kthread_create().
@@ -458,6 +461,7 @@ int kthread_park(struct task_struct *k)
 	}
 	return ret;
 }
+EXPORT_SYMBOL_GPL(kthread_park);
 
 /**
  * kthread_stop - stop a thread created by kthread_create().
