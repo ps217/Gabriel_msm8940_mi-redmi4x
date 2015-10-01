34b1eef39dc1447788aef8b819dd78f9fc108dac
Stephen Boyd <sboyd@codeaurora.org>
2015-10-01 12:22:53 -0700

    PM / Domains: Make pm_genpd_{add,remove}_subdomain() available to modules
    Export these symbols so they can be used in loadable kernel
    modules.
    
    Cc: Rob Clark <robdclark@gmail.com>
    Cc: Rajendra Nayak <rnayak@codeaurora.org>
    Cc: Kevin Hilman <khilman@kernel.org>
    Cc: Ulf Hansson <ulf.hansson@linaro.org>
    Acked-by: Rafael J. Wysocki <rjw@rjwysocki.net>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/base/power/domain.c
+++ drivers/base/power/domain.c
@@ -1348,6 +1348,7 @@ int pm_genpd_add_subdomain(struct generic_pm_domain *genpd,
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(pm_genpd_add_subdomain);
 
 /**
  * pm_genpd_remove_subdomain - Remove a subdomain from an I/O PM domain.
@@ -1394,6 +1395,7 @@ out:
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(pm_genpd_remove_subdomain);
 
 /* Default device callbacks for generic PM domains. */
 
