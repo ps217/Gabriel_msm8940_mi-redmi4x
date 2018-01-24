de7ee3971c92a66a107efdd18ee194f8e4f567f3
mostafa-z <mostafazarghami@gmail.com>
2018-01-24 07:52:08 +0330

    ramdisk:post-init: implement Google Services battery drain fixer
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- WORKING-DIR/ramdisk/ramdisk/sbin/ext/post-init.sh
+++ WORKING-DIR/ramdisk/ramdisk/sbin/ext/post-init.sh
@@ -198,4 +198,23 @@ DEBUG=/data/.gabriel/;
 BUSYBOX_VER=$(busybox | grep "BusyBox v" | cut -c0-15);
 echo "$BUSYBOX_VER" > $DEBUG/busybox_ver;
 
-$BB mount -o remount,ro /system;
+(
+	sleep 10;
+
+	# Google Services battery drain fixer by Alcolawl@xda
+	# http://forum.xda-developers.com/google-nexus-5/general/script-google-play-services-battery-t3059585/post59563859
+	pm enable com.google.android.gms/.update.SystemUpdateActivity
+	pm enable com.google.android.gms/.update.SystemUpdateService
+	pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
+	pm enable com.google.android.gms/.update.SystemUpdateService$Receiver
+	pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
+	pm enable com.google.android.gsf/.update.SystemUpdateActivity
+	pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity
+	pm enable com.google.android.gsf/.update.SystemUpdateService
+	pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver
+	pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
+
+	echo "[Gabriel-Kernel] google services fix enabled" > /dev/kmsg
+
+	$BB mount -o remount,ro /system;
+)&
