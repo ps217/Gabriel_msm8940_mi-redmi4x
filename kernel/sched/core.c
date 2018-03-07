b617b90ac133676f1164fb40864836efc27f3713
mostafa-z <mostafazarghami@gmail.com>
2018-03-08 01:39:05 +0330

    kernel: disable dmesg log related to cpuset
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4743,8 +4743,8 @@ out:
 		 * leave kernel.
 		 */
 		if (p->mm && printk_ratelimit()) {
-			printk_deferred("process %d (%s) no longer affine to cpu%d\n",
-					task_pid_nr(p), p->comm, cpu);
+			/*printk_deferred("process %d (%s) no longer affine to cpu%d\n",
+					task_pid_nr(p), p->comm, cpu);*/
 		}
 	}
 
