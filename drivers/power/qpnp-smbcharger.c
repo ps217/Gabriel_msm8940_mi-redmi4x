5b7a1ce189daf872d1a7485d73c0d7f39e15b528
mostafa-z <mostafazarghami@gmail.com>
2018-03-21 20:16:04 +0330

    power:fast_charge: allow max 2500ma draw from normal charger!
    credits to @dorimanx
    
    original commit:
    https://github.com/dorimanx/DORIMANX-OP3T-KERNEL/commit/4f3972517956fe16a97fec2e4b2bfd8a7ab5fcc0#diff-ba1a139305c66bfe5b7d6281e9969d88
    
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -443,7 +443,11 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+static int smbchg_default_hvdcp_icl_ma = 2500;
+#else
 static int smbchg_default_hvdcp_icl_ma = 2000;
+#endif
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
@@ -455,7 +459,11 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+static int smbchg_default_dcp_icl_ma = 2500;
+#else
 static int smbchg_default_dcp_icl_ma = 2000;
+#endif
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
