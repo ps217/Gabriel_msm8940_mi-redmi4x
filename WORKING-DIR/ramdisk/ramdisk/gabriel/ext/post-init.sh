8b5975428583d99c6fe89c5129c35017081b0729
mostafa-z <mostafazarghami@gmail.com>
2018-05-04 11:52:38 +0430

    ramdisk:post-init: set core_ctl max value once before loading profiles
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- WORKING-DIR/ramdisk/ramdisk/gabriel/ext/post-init.sh
+++ WORKING-DIR/ramdisk/ramdisk/gabriel/ext/post-init.sh
@@ -119,6 +119,8 @@ echo "$(cat /dev/cpuset/system-background/cpus)" > /cache/sysb_cpu;
 echo "$(cat /dev/cpuset/background/cpus)" > /cache/backg_cpu;
 
 echo 2 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus;
+echo 2 > /sys/devices/system/cpu/cpu0/core_ctl/max_cpus;
+sleep 2;
 echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/max_cpus;
 echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus;
 echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus;
@@ -321,26 +323,9 @@ CRITICAL_PERM_FIX;
 	pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver
 	pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
 
-	# make sure rom's services doesn't affect on max value.
-	# and it's done when system load has been finished.
-	BMAXCPUS="$(cat /sys/devices/system/cpu/cpu0/core_ctl/max_cpus)";
-	BMINCPUS="$(cat /sys/devices/system/cpu/cpu0/core_ctl/min_cpus)";
-	LMAXCPUS="$(cat /sys/devices/system/cpu/cpu4/core_ctl/max_cpus)";
-	LMINCPUS="$(cat /sys/devices/system/cpu/cpu4/core_ctl/min_cpus)";
-
 	while [ "$(cat /sys/class/thermal/thermal_zone5/temp)" -ge "50" ]; do
 		sleep 5;
 	done;
-
-	if [ "$(cat /sys/module/ft5x06_ts/parameters/sleep_state)" -eq "0" ]; then
-		echo $BMINCPUS > /sys/devices/system/cpu/cpu0/core_ctl/max_cpus;
-		sleep 2;
-		echo $BMAXCPUS > /sys/devices/system/cpu/cpu0/core_ctl/max_cpus;
-
-		echo $LMINCPUS > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus;
-		sleep 2;
-		echo $LMAXCPUS > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus;
-	fi;
 
 OPEN_RW;
 
