6ddf667b524c094e6dac62e5e80a4de05f303f79
mostafa-z <mostafazarghami@gmail.com>
2018-05-04 13:49:05 +0430

    cpufreq:gabriel: build alone, tune default parameters
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -517,6 +517,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_GABRIEL)
+extern struct cpufreq_governor cpufreq_gov_gabriel;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_gabriel)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CULTIVATION)
 extern struct cpufreq_governor cpufreq_gov_cultivation;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cultivation)
