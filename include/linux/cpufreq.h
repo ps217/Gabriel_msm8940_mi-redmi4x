eccbf587135f6e4b55c535f7cde613152e049891
mostafa-z <mostafazarghami@gmail.com>
2018-05-04 21:23:33 +0430

    cpufreq:gabriel.[plus]: separate gabriel custom tunables to gabriel-plus governor
    -keep idle load threshold & timer rate with gabriel too
    
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -520,6 +520,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_GABRIEL)
 extern struct cpufreq_governor cpufreq_gov_gabriel;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_gabriel)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_GABRIELPLUS)
+extern struct cpufreq_governor cpufreq_gov_gabrielplus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_gabrielplus)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CULTIVATION)
 extern struct cpufreq_governor cpufreq_gov_cultivation;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cultivation)
