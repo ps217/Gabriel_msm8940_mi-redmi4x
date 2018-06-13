0b262b7195bd57c619205b6b0de895cd954e57a7
mostafa-z <mostafazarghami@gmail.com>
2018-06-14 01:20:40 +0430

    build machine: add dtb_generator script
    -added 8937 dtsi changes as a refrence
    
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

---
+++ dtb_generator.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+RDIR=$(readlink -f .)
+WD=$RDIR/WORKING-DIR
+DTS=arch/arm/boot/dts/qcom/msm8940-pmi8950-qrd-sku7-full.dts
+TSU=$WD/anykernel/treble-supported
+TUSU=$WD/anykernel/treble-unsupported
+
+function buid_supported() {
+patch -p1 --ignore-whitespace < patch/treble.patch
+
+$RDIR/TOOLSET/dtc -I dts -O dtb -o $TSU/msm8940-pmi8950-qrd-sku7-full.dtb $DTS
+$RDIR/TOOLSET/dtc -I dts -O dtb -o $TUSU/msm8940-pmi8950-qrd-sku7-full.dtb $DTS
+
+git checkout arch/arm/boot/dts/qcom/*.dts*
+}
+
+function build_unsupported() {
+$RDIR/TOOLSET/dtc -I dts -O dtb -o $TUSU/msm8940-pmi8950-qrd-sku7-full-nt.dtb $DTS
+}
+
+function main_process() {
+buid_supported;
+build_unsupported;
+
+git add $WD/anykernel/treble-*
+git commit -m "ramdisk: regenerate device tree blobs";
+}
+
+git status;
+echo " ";
+select CHOICE in commit unstage_dtbs cancel; do
+	case "$CHOICE" in
+		"commit")
+			main_process;
+			break;;
+		"unstage_dtbs")
+			git checkout $WD/anykernel/treble-*;
+			break;;
+		"cancel")
+			break;;
+	esac;
+done;
