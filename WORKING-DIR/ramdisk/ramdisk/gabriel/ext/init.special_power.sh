99258651f5047cf3d42616c26db91f0bba8aae11
mostafa-z <mostafazarghami@gmail.com>
2018-06-13 22:45:55 +0430

    Revert "ramdisk:boot scripts: add a trigger to run only with gabriel"
    This reverts commit 94ca8a9def6b92c7309a76c87f8669a9390eaa93.

--- WORKING-DIR/ramdisk/ramdisk/gabriel/ext/init.special_power.sh
+++ WORKING-DIR/ramdisk/ramdisk/gabriel/ext/init.special_power.sh
@@ -1,8 +1,5 @@
 #!/system/bin/sh
 
-# a trigger to run only with gabriel, to not mess with other kernels
-if [ "$(cat /dev/block/bootdevice/by-name/boot | grep -m 1 sched_enable_hmp | grep gabriel | wc -l)" -gt "0" ]; then
-
 ################################################################################
 # helper functions to allow Android init like script
 
@@ -83,5 +80,3 @@ writepid_sbg $QSEEPROXYDAEMON
 writepid_sbg $CAMERASERVER
 writepid_sbg $LOGCAT
 writepid_sbg $LMKD
-
-fi;
