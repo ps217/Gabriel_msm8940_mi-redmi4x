5fc350182110e1590ae2ccf75be700a2c2696e99
mostafa-z <mostafazarghami@gmail.com>
2018-08-06 04:08:47 +0430

    time: fix/sync after merge
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- kernel/time/timer_list.c
+++ kernel/time/timer_list.c
@@ -66,7 +66,7 @@ print_timer(struct seq_file *m, struct hrtimer *taddr, struct hrtimer *timer,
 	print_name_offset(m, taddr);
 	SEQ_printf(m, ", ");
 	print_name_offset(m, timer->function);
-	SEQ_printf(m, ", S:%02lx", timer->state);
+	SEQ_printf(m, ", S:%02x", timer->state);
 	SEQ_printf(m, "\n");
 	SEQ_printf(m, " # expires at %Lu-%Lu nsecs [in %Ld to %Ld nsecs]\n",
 		(unsigned long long)ktime_to_ns(hrtimer_get_softexpires(timer)),
