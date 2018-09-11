fbc4a9b3e99c65bcfab920fde051158597437a0b
mostafa-z <mostafazarghami@gmail.com>
2018-09-11 20:17:50 +0430

    ramdisk:anykernel: prevent to edit fstab when there is no f2fs partition
    -fix miui boot failure with ext4 partition
    
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- WORKING-DIR/anykernel/anykernel.sh
+++ WORKING-DIR/anykernel/anykernel.sh
@@ -43,8 +43,7 @@ dump_boot;
 if [ -e fstab.qcom ] && [ $(mount | grep f2fs | wc -l) -gt "0" ]; then
 touch /tmp/anykernel/fstab.patch
 echo "do.fstab=1" > /tmp/anykernel/fstab.patch
-fi;
-if [ -e fstab.qcom ]; then
+
 insert_line fstab.qcom "data        f2fs" before "data        ext4" "/dev/block/bootdevice/by-name/userdata     /data        f2fs    nosuid,nodev,noatime,inline_xattr,data_flush      wait,check,encryptable=footer,formattable,length=-16384";
 insert_line fstab.qcom "cache        f2fs" after "data        ext4" "/dev/block/bootdevice/by-name/cache     /cache        f2fs    nosuid,nodev,noatime,inline_xattr,flush_merge,data_flush wait,formattable,check";
 fi;
