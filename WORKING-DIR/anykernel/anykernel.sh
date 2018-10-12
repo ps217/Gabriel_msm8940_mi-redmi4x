d094faf666d3c05dbd1300bbb82e220acabc6457
mostafa-z <mostafazarghami@gmail.com>
2018-10-12 21:45:08 +0330

    ramdisk:anykernel2: sync with dev
    -merged from gabriel hmp kernel source
    
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- WORKING-DIR/anykernel/anykernel.sh
+++ WORKING-DIR/anykernel/anykernel.sh
@@ -3,34 +3,34 @@
 
 ## AnyKernel setup
 # begin properties
-properties() {
+properties() { '
 kernel.string=Gabriel Kernel by mostafaz @ xda-developers
 do.devicecheck=1
 do.modules=1
-do.system=1
 do.cleanup=1
+do.system=1
 do.cleanuponabort=1
 do.osversion=1
 device.name1=santoni
 device.name2=Xiaomi
 device.name3=Redmi 4X
-} # end properties
+'; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
-## end setup
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
+
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
+chmod -R 755 $ramdisk/sbin;
 chown -R root:root $ramdisk/*;
 
 
@@ -65,8 +65,21 @@ fi;
 backup_file init.rc;
 insert_line init.rc "init.spectrum.rc" before "import /init.usb.rc" "import /init.spectrum.rc";
 fi;
+
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.gz
+# Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+if [ -d $ramdisk/.backup ]; then
+  ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+  $bin/magiskboot --decompress $compressed_image $decompressed_image;
+  $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D6673 77616E745F696E697472616D6673;
+  $bin/magiskboot --compress=gz $decompressed_image $compressed_image;
+  $bin/magiskboot --dtb-patch /tmp/anykernel/treble*/*;
+fi;
+
 # end ramdisk changes
 
 write_boot;
 
 ## end install
+
