882c06c21a15d9f647bdf777e03bb3f6fbc84387
mostafa-z <mostafazarghami@gmail.com>
2018-10-29 11:11:16 +0330

    Revert "dts:gpu: overclock last three frequencies"
    This reverts commit 142e204d26e670749df8c3b2242ff47496b407a1.

--- drivers/clk/msm/clock-gcc-8952.c
+++ drivers/clk/msm/clock-gcc-8952.c
@@ -787,10 +787,10 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk_8940_500MHz[] = {
 	F_SLEW( 300000000, 600000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 320000000, FIXED_CLK_SRC, gpll0,	2.5,	0,	0),
 	F_SLEW( 375000000, 750000000,	  gpll3,	1,	0,	0),
-	F_SLEW( 425000000, FIXED_CLK_SRC, gpll0,	2,	0,	0),
+	F_SLEW( 400000000, FIXED_CLK_SRC, gpll0,	2,	0,	0),
 	F_SLEW( 450000000, 900000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 475000000, 950000000,	  gpll3,	1,	0,	0),
-	F_SLEW( 575000000, 1000000000,	  gpll3,	1,	0,	0),
+	F_SLEW( 520000000, 1000000000,	  gpll3,	1,	0,	0),
 	F_END
 };
 
@@ -4423,12 +4423,12 @@ static int msm_gcc_probe(struct platform_device *pdev)
 				gfx3d_clk_src.freq_tbl =
 					ftbl_gcc_oxili_gfx3d_clk_8940_500MHz;
 				gfx3d_clk_src.c.fmax[VDD_DIG_SUPER_TUR] =
-								575000000;
+								520000000;
 			} else {
 				gfx3d_clk_src.freq_tbl =
 					ftbl_gcc_oxili_gfx3d_clk_8940_500MHz;
 				gfx3d_clk_src.c.fmax[VDD_DIG_SUPER_TUR] =
-								575000000;
+								520000000;
 			}
 		}
 	} else if (compat_bin2 || compat_bin4) {
