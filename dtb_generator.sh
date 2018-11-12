547d8272aecfef00dbaac4fb91a989848d9317c5
mostafa-z <mostafazarghami@gmail.com>
2018-11-12 12:36:08 +0330

    dtb_gen: add 66hz framerate build rule
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- dtb_generator.sh
+++ dtb_generator.sh
@@ -5,12 +5,28 @@ WD=$RDIR/WORKING-DIR
 DTS=arch/arm/boot/dts/qcom/msm8940-pmi8950-qrd-sku7-full.dts
 TSU=$WD/anykernel/treble-supported
 TUSU=$WD/anykernel/treble-unsupported
+# 66hz freamerate
+TSU66=$WD/anykernel/treble-supported-66
+TUSU66=$WD/anykernel/treble-unsupported-66
 
 function buid_supported() {
+if [ ! -d $TSU ] || [ ! -d $TSU66 ]; then
+	mkdir -p $TSU;
+	mkdir -p $TUSU;
+	mkdir -p $TSU66;
+	mkdir -p $TUSU66;
+fi;
+
 patch -p1 --ignore-whitespace < patch/treble.patch
 
 $RDIR/TOOLSET/dtc -I dts -O dtb -o $TSU/msm8940-pmi8950-qrd-sku7-full.dtb $DTS
 $RDIR/TOOLSET/dtc -I dts -O dtb -o $TUSU/msm8940-pmi8950-qrd-sku7-full.dtb $DTS
+
+# 66hz freamerate
+patch -p1 --ignore-whitespace < patch/66hz.patch
+
+$RDIR/TOOLSET/dtc -I dts -O dtb -o $TSU66/msm8940-pmi8950-qrd-sku7-full.dtb $DTS
+$RDIR/TOOLSET/dtc -I dts -O dtb -o $TUSU66/msm8940-pmi8950-qrd-sku7-full.dtb $DTS
 
 rm $DTS*.orig
 git checkout arch/arm/boot/dts/qcom/*.dts*
@@ -18,6 +34,9 @@ git checkout arch/arm/boot/dts/qcom/*.dts*
 
 function build_unsupported() {
 $RDIR/TOOLSET/dtc -I dts -O dtb -o $TUSU/msm8940-pmi8950-qrd-sku7-full-nt.dtb $DTS
+
+# 66hz freamerate
+$RDIR/TOOLSET/dtc -I dts -O dtb -o $TUSU66/msm8940-pmi8950-qrd-sku7-full-nt.dtb $DTS
 }
 
 function commit_process() {
